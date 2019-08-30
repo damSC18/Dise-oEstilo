4e39e3f9adef7097687e091ea17b09b3124fb32a
unknown <trabajodam18@gmail.com>
2019-08-30 16:44:32 +0200

    Proyecto WPF -- Curso 2019/2020

---
+++ Dise-oEstilo/DiseñoEstilo/BarraDeHerramientas.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace DiseñoEstilo
+{
+    /// <summary>
+    /// Lógica de interacción para BarraDeHerramientas.xaml
+    /// </summary>
+    public partial class BarraDeHerramientas : Window
+    {
+        public BarraDeHerramientas()
+        {
+            InitializeComponent();
+        }
+
+        private void CerrarVentana(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void MenuMd(object sender, RoutedEventArgs e)
+        {
+            MenuMD mmd = new MenuMD();
+            mmd.Show();
+        }
+    }
+}
---
+++ Dise-oEstilo/DiseñoEstilo/MenuMD.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace DiseñoEstilo
+{
+    /// <summary>
+    /// Lógica de interacción para MenuMD.xaml
+    /// </summary>
+    public partial class MenuMD : Window
+    {
+        public MenuMD()
+        {
+            InitializeComponent();
+        }
+
+        private void Volver(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Copy(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Conmando");
+        }
+    }
+}
